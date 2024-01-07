0f56667221c2c8086cbfdabc1601b4694d43c439
LY <51789698+Young-Lord@users.noreply.github.com>
2024-01-07 16:47:31 +0800

    feat!: enable CORS for all requests

--- server/app/views/view_api.py
+++ server/app/views/view_api.py
@@ -1,9 +1,8 @@
 from app.note_const import Metadata
-from app.utils import cors_decorator, return_json
+from app.utils import return_json
 from app.resources.base import api_bp as api
 
 
 @api.route("/metadata")
-@cors_decorator
 def api_metadata():
     return return_json(data=Metadata.to_dict())
