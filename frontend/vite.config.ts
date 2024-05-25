a13bfd1a61e982da0359a551448c697de11160fe
LY <51789698+Young-Lord@users.noreply.github.com>
2024-05-25 13:59:47 +0800

    chore: remove useless import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite"
 
 // Utilities
-import { createLogger, defineConfig, loadEnv } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import { fileURLToPath, URL } from "node:url"
 // import { visualizer } from "rollup-plugin-visualizer"
 
