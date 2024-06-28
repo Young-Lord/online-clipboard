5171254fc355082716942973600653dd47cbb002
LY <51789698+Young-Lord@users.noreply.github.com>
2024-06-28 13:52:21 +0800

    refactor: reimplement static file hosting

--- server/app/views/frontend.py
+++ server/app/views/frontend.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+from typing import Final
 from flask import Blueprint, render_template, send_from_directory
 
-templates_folder_name = "templates"
+templates_folders: Final[list[str]] = ["app/templates", "server/app/templates"]
+templates_folder: Final[Path] = Path(
+    next(i for i in templates_folders if Path(i).is_dir())
+).absolute()
+templates_folder_files: Final[set[str]] = set(i.name for i in templates_folder.iterdir() if i.is_file())
 frontend = Blueprint(
     "frontend",
     "frontend",
-    static_folder=f"app/{templates_folder_name}/static",
-    template_folder=f"app/{templates_folder_name}",
+    static_folder=templates_folder,
+    static_url_path="/",
+    template_folder=templates_folder,
 )
 
 
@@ -14,15 +21,8 @@ def index():
     return render_template("index.html")
 
 
-@frontend.route("/favicon.ico")
-def favicon():
-    return send_from_directory(
-        templates_folder_name,
-        "favicon.ico",
-        mimetype="image/vnd.microsoft.icon",
-    )
-
-
 @frontend.route("/<clip_name_input>")
-def clip_name(*args, **kwargs):
+def clip_name(clip_name_input: str):
+    if clip_name_input in templates_folder_files:
+        return send_from_directory(templates_folder, clip_name_input)
     return render_template("index.html")
