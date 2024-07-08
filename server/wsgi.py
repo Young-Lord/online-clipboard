af0217dc1470d663b7eba26e3e80dfd41ba2ed9a
LY <51789698+Young-Lord@users.noreply.github.com>
2024-07-08 21:10:30 +0800

    fix: flask command not working with argparse

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,24 +1,32 @@
 from io import TextIOWrapper
 from os import environ
+from os.path import basename
 import argparse
+from sys import argv
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--export-metadata",
     help="Export metadata.json to file",
     type=argparse.FileType("w", encoding="UTF-8"),
-    metavar="FILENAME"
+    metavar="FILENAME",
 )
-args = parser.parse_args()
 
-export_file: TextIOWrapper = args.export_metadata
-if export_file:
-    from app.note_const import Metadata_dict
-    import json
 
-    json.dump(Metadata_dict, export_file)
-    exit(0)
+def parse_custom_args() -> None:
+    args = parser.parse_args()
+    export_file: TextIOWrapper = args.export_metadata
+    if export_file:
+        from app.note_const import Metadata_dict
+        from json import dump
 
+        dump(Metadata_dict, export_file)
+        exit(0)
+
+
+if basename(argv[0]) != "flask":
+    # special case for running flask command
+    parse_custom_args()
 
 if environ.get("VERCEL", "0") != "1":
     # need running server
@@ -27,6 +35,7 @@ if environ.get("VERCEL", "0") != "1":
     monkey.patch_all()
 
 from app.main import create_app
+
 app = create_app()
 
 from app.resources.socketio import socketio
